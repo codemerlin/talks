f5ae12c504d20c78363c70222323a6ac7898f493
Mohit Thakral <mohit@thakral.in>
2013-11-28 06:50:58 -0600

    Finished till services

---
+++ barcampdelhi-winter-30Nov2013/code/app/js/directive.js
@@ -0,0 +1,12 @@
+﻿//define Module
+var myAppModule = angular.module('myApp', []);
+
+//define Controller
+myAppModule.controller("PersonController"
+, [function () {
+    this.name = "John";
+}]);
+
+myAppModule.directive("ngRedText", function () {
+    
+});
